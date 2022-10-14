3fcb91f7ad5ee268c064ab5ecc63c9f82332af39
Bruno Bispo <brunobispoo25@gmail.com>
2022-10-13 23:16:22 -0300

    SOLID 02 - Criação de uma classe sem principios do SOLID.

---
+++ src/shopping-cart.ts
@@ -0,0 +1,73 @@
+interface CartItem {
+  name: string;
+  price: number;
+}
+
+type OrderStatus = 'open' | 'closed';
+
+export class ShoppingCart {
+  private readonly _items: CartItem[] = [];
+  private _orderStatus: OrderStatus = 'open';
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.slice(index, 1);
+  }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  get orderStatus(): Readonly<OrderStatus> {
+    return this._orderStatus;
+  }
+
+  total(): number {
+    return +this._items.reduce((acc, next) => acc + next.price, 0).toFixed(2);
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Seu carrrinho está vazio');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.sendMessage(`Seu pedido com total de ${this.total()} foi recebido`);
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  sendMessage(msg: string): void {
+    console.log(`Mensagem enviada: ${msg}`);
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso...');
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras foi limpo');
+    this._items.length = 0;
+  }
+}
+
+const shoppingCart = new ShoppingCart();
+
+shoppingCart.addItem({ name: 'Camisa', price: 19.99 });
+shoppingCart.addItem({ name: 'Camiseta', price: 10.99 });
+shoppingCart.addItem({ name: 'Short', price: 29.99 });
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.orderStatus);
+shoppingCart.checkout();
+
+console.log(shoppingCart.orderStatus);
