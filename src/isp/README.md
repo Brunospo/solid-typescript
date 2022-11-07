70daca42796edfc51da7bac96980298b2eefbe61
Bruno Bispo <brunobispoo25@gmail.com>
2022-11-07 20:58:51 -0300

    SOLID 05 - Implementando o principio Interface Segregation do SOLID.

---
+++ src/isp/README.md
@@ -0,0 +1,3 @@
+# ISP - Interface Segregation Principle
+
+Principio da segregação de interface: Os Clientes não devem ser forçados a depender de types, interfaces ou membros abstratos que não utilizam. Ou seja, não devemos criar interfaces muito infladas, com muitas coisas fazendo com que outras classes seja obrigadas a implementar coisas que elas não vão utilizar.
---
+++ src/isp/customer-protocol.ts
@@ -0,0 +1,22 @@
+/*Essa interface quebra o pricipio ISP, pois essa interface faz com que
+quem a implementa seja obrigado a implementar coisas que ela não vai utilizar. */
+
+export interface CustomerProtocol {
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+  cnpj: string;
+}
+
+// Essas interfaces são mais recomendadas
+
+export interface IndividualCustomerProtocol {
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+}
+
+export interface EnterpriseCustomerProtocol {
+  nome: string;
+  cnpj: string;
+}
